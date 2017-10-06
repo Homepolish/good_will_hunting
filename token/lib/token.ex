20f7901d073baf1a06b44c39877dd2bf2846c563
Erik Reedstrom <erik.reedstrom@gmail.com>
2017-10-06 13:09:31 +0200

    feat: add token question

---
+++ token/lib/token.ex
@@ -0,0 +1,33 @@
+defmodule Token do
+  @moduledoc false
+
+  @doc """
+  Verifies a token for a resource.
+
+  ## Examples
+
+      iex> Token.token_valid?("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiYXBpIl0sImV4cCI6MTUwOTcwNTcyOSwiaWF0IjoxNTA3Mjg2NTI5LCJpc3MiOiJob21lcG9saXNoLmNvbSIsImp0aSI6IjhiMjY2OWNmLWEwNzItNDE0Zi1iMjgwLTcxYTU0MTFlZjc4OSIsIm5iZiI6MTUwNzI4NjUyOCwic3ViIjoiRGFsdG9uIiwidHlwIjoiYWNjZXNzIn0.a-bWuUABLoVrDo0EkJ_RWxYCPjBqfecdKW1ScJ1tFQ8")
+      true
+
+  """
+  @spec token_valid?(String.t) :: boolean
+  def token_valid?(token) do
+    :not_implemented
+  end
+
+  @doc """
+  Creates a token for a resource.
+
+  ## Examples
+
+      iex> {:ok, _token, claims} = Token.get_token("Dalton")
+      iex> {claims, _} = Map.split(claims, ["aud", "sub", "typ"])
+      iex> claims
+      %{"aud" => ["api"], "sub" => "Dalton", "typ" => "refresh"}
+
+  """
+  @spec get_token(String.t) :: {:ok, String.t, map}
+  def get_token(resource) do
+    :not_implemented
+  end
+end
---
+++ token/test/token_test.exs
@@ -0,0 +1,5 @@
+defmodule TokenTest do
+  use ExUnit.Case
+  doctest Token
+
+end
