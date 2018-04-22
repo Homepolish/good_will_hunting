96b4f99c6ed3cca6be23d504216227108196ef1d
Give money to charity <henrysavit@gmail.com>
2018-04-22 12:57:10 -0400

    finished

--- password/mix.exs
+++ password/mix.exs
@@ -21,6 +21,9 @@ defmodule Password.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:pbkdf2_elixir, "~> 0.12"}, #SHA256 referenced in the comeonin repo. this didnt work
+      {:bcrypt_elixir, "~> 1.0.6"} #this one works. referenced in the comeonin repo https://github.com/riverrun/comeonin
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
--- queries/lib/queries.ex
+++ queries/lib/queries.ex
@@ -1,18 +1,28 @@
 defmodule Queries do
   @moduledoc false
-
+  import Ecto.Query
+  alias Queries.{Repo, City, Weather}
+  
   @doc """
   Returns list of cities with temp >= 24c and temp <= 30c, and pop > 1_000_000.
   """
   def big_cities_with_perfect_weather() do
-    :not_implemented
+    Repo.all(
+      from city in City,
+      join: temp in Weather, on: temp.city_id == city.id,
+      where: temp.temp_lo >= 24 and temp.temp_hi <= 30 and city.population > 1_000_000
+    )  
   end
 
   @doc """
   Returns list of cities with temp < 24c, or pop < 5_000.
   """
   def small_cities_or_cold_cities() do
-    :not_implemented
+    Repo.all(
+      from city in City,
+      join: temp in Weather, on: temp.city_id == city.id,
+      where: temp.temp_lo < 24 or city.population < 5_000
+    )
   end
 
   @doc """
@@ -22,6 +32,11 @@ defmodule Queries do
   Note: Try something other than `or` clauses for this query.
   """
   def medium_cities_or_hot_cities_or_cold_small_cities() do
-    :not_implemented
+    City
+    |> join(:inner, [city], temp in Weather, city.id == temp.city_id)
+    |> where([city], city.population > 5_000 and city.population < 1_000_000)
+    |> or_where([city, temp], temp.temp_lo > 30)
+    |> or_where([city, temp], city.population < 5_000 and temp.temp_lo < 24)
+    |> Repo.all
   end
 end
